c928c1ca7ee18c756a3ddfc913318dd92d405342
KodeKenobi <kodekenobi@gmail.com>
2025-01-09 18:36:51 +0200

    Introduced a .env file for all our environment variables

--- app/(auth)/firebaseConfig.ts
+++ app/(auth)/firebaseConfig.ts
@@ -3,16 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_N1k2JZCJu6xnqGqZayLvE5LNKDF3oKI",
-  authDomain: "wellness-67f28.firebaseapp.com",
-  databaseURL: "https://wellness-67f28-default-rtdb.firebaseio.com",
-  projectId: "wellness-67f28",
-  storageBucket: "wellness-67f28.appspot.com",
-  messagingSenderId: "1018888888888",
-  appId: "1:1018888888888:web:1234567890",
-  measurementId: "G-measurement-id",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);

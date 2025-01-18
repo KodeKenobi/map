f3ba97bc941f236d416f02da0a8c93bf4045699d
KodeKenobi <kodekenobi@gmail.com>
2025-01-18 20:59:52 +0200

    Project Restructure

--- react-native-vector-icons.d.ts
+++
@@ -1,5 +0,0 @@
-// @types/react-native-vector-icons.d.ts
-declare module "react-native-vector-icons/MaterialIcons" {
-  import { Icon } from "react-native-vector-icons/Icon";
-  export default Icon;
-}
--- react-native.config.ts
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  assets: ["./assets/fonts", "node_modules/@ant-design/icons-react-native/fonts"],
-};
